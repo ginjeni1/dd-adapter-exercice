2fbf22c18fa821158a7771d582b6148bd17b71be
ginjeni1 <jenifael.gingras-courchesne@optelgroup.com>
2018-12-19 14:17:33 -0500

    refactor(event/logger): extract validation method

--- sdk/eventlogger.py
+++ sdk/eventlogger.py
@@ -8,15 +8,22 @@ class StringEvent(object):
         self.msg = msg
 
 
+def validate_event(event):
+    if not isinstance(event.timestamp, int):
+        raise ValueError("timestamp must be an integer")
+    if not isinstance(event.msg, str):
+        raise ValueError("msg must be an integer")
+
+
 class EventLogger(object):
     """
     EventLogger will write into the event buffer the events it receives.
     """
+    def __init__(self, validate=validate_event):
+        self.validate = validate
+
     def receive(self, event):
-        if not isinstance(event.timestamp, int):
-            raise ValueError("timestamp must be an integer")
-        if not isinstance(event.msg, str):
-            raise ValueError("msg must be an integer")
+        self.validate(event)
 
         print("Event : {} : {}".format(
             event.timestamp,
